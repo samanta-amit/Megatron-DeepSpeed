82555c8ed745ba8f92c4fa1515bd34ccdd743266
Sam Foreman <saforem2@gmail.com>
2024-02-05 16:45:58 -0600

    Update `set_params.sh`

--- set_params.sh
+++ set_params.sh
@@ -13,6 +13,7 @@ export TP=${TP:-1}
 export PP=${PP:-1}
 export GRAD_ACC_STEPS=${GRAD_ACC_STEPS:-1}
 export GLOBAL_BATCH=$(( $WORLD_SIZE * $MICRO_BATCH * $GRAD_ACC_STEPS / $TP / $PP ))
+export USE_ACTIVATION_CHECKPOINTING=${USE_ACTIVATION_CHECKPOINTING:-0}
 
 
 # bash $LLM_DK_DIR/intel-extension-for-deepspeed/examples/gpt.sh $@
@@ -21,20 +22,25 @@ export GLOBAL_BATCH=$(( $WORLD_SIZE * $MICRO_BATCH * $GRAD_ACC_STEPS / $TP / $PP
 TP=${TP:-1}
 PP=${PP:-1}
 
-# export DATA_PARENT="/home/foremans/polaris/projects/saforem2/Megatron-DeepSpeed"
+export DATA_PARENT="/home/foremans/polaris/projects/saforem2/Megatron-DeepSpeed"
 # export DATA_TYPE="BookCorpusDataset_text_document"
-export DATA_PARENT="/lus/eagle/projects/datasets/Megatron-DeepSpeed/GenSLMSubSample200k" 
-export DATA_TYPE="genslm_subsample_200k_sequence_document" 
+# export DATA_PARENT="/lus/eagle/projects/datasets/Megatron-DeepSpeed/GenSLMSubSample200k" 
+# export DATA_TYPE="genslm_subsample_200k_sequence_document" 
 export DATA_DIR="${DATA_PARENT}/dataset" 
-export DATA_PATH="${DATA_DIR}/${DATA_TYPE}"
+# export DATA_PATH="${DATA_DIR}/${DATA_TYPE}"
 export VOCAB_FILE="${DATA_DIR}/gpt2-vocab.json" 
 export MERGE_FILE="${DATA_DIR}/gpt2-merges.txt"
+
+export DATA_PATH="/eagle/datasets//dolma/data_Llama2Tokenizer/wiki-en-simple/"
+export DATA_FILE_LIST="/eagle/datasets//dolma//data_file_list_small.txt"
 
 
 DS_CONFIG="ds_stage${ZERO_STAGE}_mb${MICRO_BATCH}_gb${GLOBAL_BATCH}_pp${PP}_${DTYPE}.json"
 bash ./generate_config.sh ${DS_CONFIG} || exit 1
 
-OUTPUT_DIR=logs/ds_stage${ZERO_STAGE}_nl${NLAYERS}_hs${HIDDEN}_mb${MICRO_BATCH}_seq${SEQ}_gb${GLOBAL_BATCH}_pp${PP}_tp${TP}_${DTYPE}_`date +%m%d%H%M%S`_${HOSTNAME}
+OUTPUT_PREFIX="logs/ds_stage${ZERO_STAGE}_nl${NLAYERS}_hs${HIDDEN}_mb${MICRO_BATCH}_seq${SEQ}_gb${GLOBAL_BATCH}_pp${PP}_tp${TP}_${DTYPE}"
+# OUTPUT_DIR=logs/ds_stage${ZERO_STAGE}_nl${NLAYERS}_hs${HIDDEN}_mb${MICRO_BATCH}_seq${SEQ}_gb${GLOBAL_BATCH}_pp${PP}_tp${TP}_${DTYPE}_`date +%m%d%H%M%S`_${HOSTNAME}
+OUTPUT_DIR="${OUTPUT_PREFIX}/$(date +%m%d%H%M%S)_${HOSTNAME}"
 mkdir -p $OUTPUT_DIR
 echo "!!!Please see logs at ${OUTPUT_DIR}"
 
@@ -51,8 +57,27 @@ if [ $PP == 1 ]; then
 fi
 ds_args=" --deepspeed_config=$DS_CONFIG ${ds_args}"
 ds_args=" --zero-stage=$ZERO_STAGE ${ds_args}"
+
+if [[ "$USE_ACTIVATION_CHECKPOINTING" == 1 ]]; then
+    echo "!! Caught USE_ACTIVATION_CHECKPOINTING=${USE_ACTIVATION_CHECKPOINTING} !!"
+    ds_args=" --deepspeed-activation-checkpointing ${ds_args}"
+    # --checkpoint-activations \
+    # --deepspeed-activation-checkpointing
+fi
+
+gpt_args=()
+
+if [[ "$USE_ACTIVATION_CHECKPOINTING" == 1 ]]; then
+    echo "!! Caught USE_ACTIVATION_CHECKPOINTING=${USE_ACTIVATION_CHECKPOINTING} !!"
+    gpt_args+=(
+        "--checkpoint-activations"
+        "--checkpoint-num-layers 1"
+    )
+fi
 # we are now using activation checkpoint provided by megatron, see below.
 # ds_args=" --deepspeed-activation-checkpointing ${ds_args}"
+# NUM_KV_HEADS="${NUM_KV_HEADS:-0}"
+# if [[ $NUM_KV_HEADS -]]
 
 # take custom args
 custom_args=" $@"
@@ -66,6 +91,9 @@ else
 fi
 
 NCCL=${NCCL:-nccl}
+
+# MODEL=LLAMA_7B
+# OUTPUT_PREFIX=${MODEL}_z${ZERO_STAGE}_seqlen_tp${TP}_pp${PP}_sp${SP}_nl${NUM_LAYERS}_hs${HIDDEN_SIZE}_gb${BS}_mb${MBS}
 
 run_cmd="
     deepspeed $launcher pretrain_gpt_alcf.py \
@@ -86,14 +114,9 @@ run_cmd="
     --log-interval 1 \
     --eval-iters 100 \
     --eval-interval 100 \
-    --data-path $DATA_PATH \
-    --vocab-file $VOCAB_FILE \
-    --merge-file $MERGE_FILE \
-    --save-interval 500 \
+    --save-interval 50 \
     --split 100,0,0 \
     --$DTYPE \
-    --checkpoint-activations \
-    --deepspeed-activation-checkpointing
     $ds_args \
     --no-masked-softmax-fusion \
     --no-bias-gelu-fusion \
@@ -101,6 +124,15 @@ run_cmd="
     --no-gradient-accumulation-fusion \
     --distributed-backend $NCCL \
     --num-workers 0 \
+    --tokenizer-type Llama2Tokenizer \
+    --save checkpoints/${OUTPUT_PREFIX} \
+    --load checkpoints/${OUTPUT_PREFIX} \
+    --tokenizer-model /eagle/datasets/dolma/utils/tokenizer.model \
+    --data-file-list ${DATA_FILE_LIST} \
+    --data-path $DATA_PATH \
+    --vocab-file $VOCAB_FILE \
+    --merge-file $MERGE_FILE \
+    ${gpt_args[*]} \
     $custom_args \
     |& tee $OUTPUT_DIR/output.log
     "
