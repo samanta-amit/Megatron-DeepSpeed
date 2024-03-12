d91b237ba1460556f96c04e22d766b741d190e7d
Huihuo Zheng <huihuo.zheng@anl.gov>
2024-03-12 01:12:14 -0500

    changed script to common environement

--- ALCF/test_blendable_dataset.py
+++ ALCF/test_blendable_dataset.py
@@ -5,12 +5,14 @@ from megatron.global_vars import set_args, set_global_variables, get_args
 from megatron.arguments import parse_args 
 from megatron.initialize import initialize_megatron
 from megatron.data.data_samplers import build_pretraining_data_loader
-
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
 initialize_megatron(allow_no_cuda=True)
 args = get_args()
 
 data_file_list = args.data_file_list
-print(f"Reading data from {args.data_file_list}")
+if comm.rank==0:
+    print(f"Reading data from {args.data_file_list}")
 files = []
 weights = []
 flist = []
@@ -28,10 +30,10 @@ weights = weights/np.sum(weights)
 
 num_samples = args.global_batch_size*args.train_iters
 num_datasets = len(weights)
-
-print(f"Number of datasets: {num_datasets}")
-print(f"Global batch size: {args.global_batch_size}")
-print(f"Training iterations: {args.train_iters}")
+if comm.rank==0:
+    print(f"Number of datasets: {num_datasets}")
+    print(f"Global batch size: {args.global_batch_size}")
+    print(f"Training iterations: {args.train_iters}")
 train_valid_test_num_samples = [num_samples, 0, 0]
 seed=args.seed
 data_impl = args.data_impl
@@ -48,21 +50,23 @@ dataset_idx = [train_ds.dataset_index[i] for i in range(num_samples)]
 ratio_select=np.zeros(num_datasets)
 #for i in range(num_datasets):
 #    ratio_select[i] = np.sum([i==d for d in dataset_idx])/num_samples
-
-print(f"Total number of samples: {len(train_ds)}")
-print(f"Weights set: {weights[:min(8, num_datasets)]}")
+if comm.rank ==0:
+    print(f"Total number of samples: {len(train_ds)}")
+    print(f"Weights set: {weights[:min(8, num_datasets)]}")
 #print(f"Weights across training: {ratio_select[:min(8, num_datasets)]}")
 
 for e in range(min(100, args.train_iters)):
     ratio_select=np.zeros(num_datasets)
     for i in range(num_datasets):
         ratio_select[i] = np.sum([i==d for d in dataset_idx[e*args.global_batch_size:(e+1)*args.global_batch_size]])/args.global_batch_size
-    print(f"iter-{e}: {ratio_select[:min(8, num_datasets)]}")
+    if comm.rank==0:
+        print(f"iter-{e}: {ratio_select[:min(8, num_datasets)]}")
 
 
 print("First 10 samples")
 for i in range(10):
-    print(f"Sample: {i} \t dataset_idx: {train_ds.dataset_index[i]}, sample_idx: {train_ds.dataset_sample_index[i]}")
+    if comm.rank==0:
+        print(f"Sample: {i} \t dataset_idx: {train_ds.dataset_index[i]}, sample_idx: {train_ds.dataset_sample_index[i]}")
 
 
 #### Build data loaders
