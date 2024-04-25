58cdccaa2438ac1da2a310652df3dc410e7a1138
Sam Foreman <saforem2@gmail.com>
2024-04-24 19:17:30 -0500

    Update `ALCF/test_sirius.sh`

--- ALCF/test_sirius.sh
+++ ALCF/test_sirius.sh
@@ -2,7 +2,14 @@
 #
 # Run complete test of
 # https://github.com/argonne-lcf/Megatron-DeepSpeed
-# on Sunspot @ ALCF
+# on Sirius @ ALCF
+# to launch (inside an interactive `qsub -I` job) on Sirius:
+#
+# ```bash`
+# $ git clone https://github.com/argonne-lcf/Megatron-DeepSpeed
+# $ cd Megatron-DeepSpeed/ALCF
+# $ bash test_sirius.sh
+# ````
 
 # EXIT ON ERROR(s)
 set -euxo pipefail
@@ -18,12 +25,6 @@ setup_conda() {
     shell_name=$(echo "${SHELL}" | tr "\/" "\t" | awk '{print $NF}')
     eval "$("${MAMBA_ROOT_PREFIX}/bin/micromamba" shell hook --shell ${shell_name})"
     micromamba activate 2024-04-23
-    # if [[ "${SHELL}" = "/bin/zsh" ]]; then
-    #     eval "$(~/miniconda3/bin/conda shell.zsh hook)"
-    # else
-    #     eval "$(~/miniconda3/bin/conda shell.bash hook)"
-    # fi
-    # conda activate q4-drop
 }
 
 
@@ -32,7 +33,7 @@ setup_conda() {
 # does not already exist
 ########################################
 setup_megatron_deepspeed() {
-    OUTDIR="test-sirius-${NOW}" && mkdir -p "${OUTDIR}" && cd "${OUTDIR}"
+    OUTDIR="OUTPUTS/test-sirius-${NOW}" && mkdir -p "${OUTDIR}" && cd "${OUTDIR}"
     echo "Running test in: ${OUTDIR}"
     echo "WORKING DIRECTORY: $(realpath $(pwd .))"
     if [[ -d "Megatron-DeepSpeed" ]]; then
@@ -49,18 +50,17 @@ setup_megatron_deepspeed() {
 main() {
     setup_conda
     setup_megatron_deepspeed
-    # NOTE: to use OPT=adamwschedulefree, you will need to pip install schedulefree
-    DEBUG=1
-    PBS_O_WORKDIR="$(pwd)"
-    DATA_FILE_LIST=./ALCF/data-lists/sirius/books.txt
+    export DEBUG=1
+    export PBS_O_WORKDIR="$(pwd)"
+    export DATA_FILE_LIST=./ALCF/data-lists/sirius/books.txt
     # LR=0.0008
     # GRAD_ACC_STEPS=8
-    ZERO_STAGE=1
-    NUM_LAYERS=10
-    MICRO_BATCH=8
-    # OPT=adamwschedulefree
-    TIMING_LOG_LEVEL=1
-    bash train_llama_alcf.sh |& tee test-sirius.log
+    export ZERO_STAGE=1
+    export NUM_LAYERS=10
+    export MICRO_BATCH=8
+    export TRAIN_ITERS=20
+    export TIMING_LOG_LEVEL=1
+    bash train_llama_alcf.sh |& tee "test-sirius-${NOW}".log
 }
 
 main
