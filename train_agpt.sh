2a49f6dcb55771fba1b8737060848a4f04ecdd2f
Sam Foreman <saforem2@gmail.com>
2024-04-26 13:59:49 -0500

    Update `train_agpt.sh`

--- train_agpt.sh
+++ train_agpt.sh
@@ -8,4 +8,5 @@ mkdir -p "${OUTDIR}"
 OUTFILE="${OUTDIR}/${PBS_JOBID}-${NOW}.log"
 echo "${OUTFILE}" >> "${OUTDIR}/latest"
 echo "Logging job output to: ${OUTFILE}"
+# export DEBUG=1
 bash "${PBS_O_WORKDIR}/train_llama_alcf.sh" |& tee "${OUTFILE}"
