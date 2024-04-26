e31bb23b9c7e865b873c9d888439ba81018ba387
Sam Foreman <saforem2@gmail.com>
2024-04-26 12:15:24 -0500

    Add `train_agpt.sh`

---
+++ train_agpt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash --login
+
+NOW="$(date "+%Y-%m-%d-%H%M%S")"
+cd "${PBS_O_WORKDIR}" || exit
+
+OUTDIR="${PBS_O_WORKDIR}/pbslogs"
+mkdir -p "${OUTDIR}"
+OUTFILE="${OUTDIR}/${PBS_JOBID}-${NOW}.log"
+echo "${OUTFILE}" >> "${OUTDIR}/latest"
+echo "Logging job output to: ${OUTFILE}"
+bash "${PBS_O_WORKDIR}/train_llama_alcf.sh" |& tee "${OUTFILE}"
