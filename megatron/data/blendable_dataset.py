fixed overflow issue

megatron/data/blendable_dataset.py
@@ -12,7 +12,7 @@ import torch
 from deepspeed.accelerator import get_accelerator
 from megatron import print_rank_0, print_flush
 from megatron.core import mpu
-
+from mpi4py import MPI
 class BlendableDataset(torch.utils.data.Dataset):
 
 
@@ -52,8 +52,8 @@ class BlendableDataset(torch.utils.data.Dataset):
         desc += f"Weights: {weights}\n"
         desc += f"Size: {size}\n"
         self.desc = desc
-        self.dataset_index = []
-        self.dataset_sample_index = []
+        self.dataset_index = np.zeros(self.size, dtype=np.int64)
+        self.dataset_sample_index = np.zeros(self.size, dtype=np.int64)
         if data_cache_path:
             desc_hash = hashlib.md5(desc.encode('utf-8')).hexdigest()
             desc_path = os.path.join(data_cache_path, desc_hash + ".dsc")
@@ -66,12 +66,15 @@ class BlendableDataset(torch.utils.data.Dataset):
                       ' dataset, building indices on rank 0 ...', flush=True)
                 dataset_index, dataset_sample_index = _build_indices()
                 try:
+                    print_rank_0(" > saving index map files")
+                    start_time = time.time()
                     os.makedirs(os.path.dirname(index_path), exist_ok=True)
                     with open(desc_path, 'wt') as fd:
                         fd.write(desc)
                         np.save(index_path, dataset_index, allow_pickle=True)
                         np.save(sample_index_path, dataset_sample_index,
                                 allow_pickle=True)
+                    print_rank_0(f" > finished saving index map files in {time.time() - start_time} seconds")                        
                 except OSError:
                     print(f'There was an error trying to create the data cache directory ({data_cache_path})')
                     print('or a file in it. This is set with the --data-cache-path argument. Please')
@@ -90,17 +93,15 @@ class BlendableDataset(torch.utils.data.Dataset):
                 print_rank_0("Data index creation unsuccessful, exiting.")
                 exit()
 
-            if cache_hit:
-                print_rank_0(f"> index map files exists already, rank 0 will read them and broadcast to all the rank")
-                print_rank_0(f'> loading blendable dataset index: {index_path}')
-                self.dataset_index = np.load(index_path, allow_pickle=True, mmap_mode='r')
-                assert self.dataset_index.size == self.size
-
-                print_rank_0(f'> loading blendable dataset sample index: {sample_index_path}')
-                self.dataset_sample_index = np.load(sample_index_path, allow_pickle=True, mmap_mode='r')
-                assert self.dataset_sample_index.size == self.size
-            torch.distributed.broadcast_object_list(self.dataset_sample_index, src=0)
-            torch.distributed.broadcast_object_list(self.dataset_index, src=0)                
+            start_time = time.time()
+            print_rank_0(f'> loading blendable dataset index: {index_path}')
+            self.dataset_index = np.load(index_path, allow_pickle=True, mmap_mode='r')
+            assert self.dataset_index.size == self.size
+            print_rank_0(f'> loading blendable dataset sample index: {sample_index_path}')
+            self.dataset_sample_index = np.load(sample_index_path, allow_pickle=True, mmap_mode='r')
+            assert self.dataset_sample_index.size == self.size
+            torch.distributed.barrier()
+            print_rank_0(f'> finished loading in {time.time() - start_time} seconds')            
         else:
             self.dataset_index, self.dataset_sample_index = _build_indices()
 

megatron/data/gpt_dataset.py
@@ -94,11 +94,17 @@ def build_train_valid_test_datasets(data_prefix, data_impl, splits_string,
                     self.desc += dataset_builders[i].prefix + ","
                     for j in range(dataset_builders[i].num_samples):
                         self.indices.append((i, j))
+                assert(len(self.indices)==self.num_samples)
                 self.desc += f"-{self.num_samples}" + f"-{dataset_builders[0].seq_length}" + f"{dataset_builders[0].seed}"
             def __len__(self):
                 return self.num_samples
             def __getitem__(self, idx):
-                i, j = self.indices[idx]
+                if idx >= self.num_samples:
+                    print_flush(f"WARNING: index overflow encountered {idx} > {self.num_samples} for {self.dataset_builders[0].corpus}; will randomly pick one sample")
+                    id = np.random.randint(self.num_samples)
+                else:
+                    id = idx
+                i, j = self.indices[id]
                 if self.dataset_builders[i].build:
                     return self.dataset_builders[i].dataset[j]
                 else:
@@ -133,10 +139,18 @@ def build_train_valid_test_datasets(data_prefix, data_impl, splits_string,
             for i, d in zip(range(len(weights)), dataset_builders):
                 corpus_builders[d.corpus].append(d)
                 corpus_weights[d.corpus] += weights[i]
+            total = 0
+            print_rank_0(" > number of samples for each corpus ")
+            corpus_weights_achieved={}
             for c in corpus_list:
                 datasets.append(BuildConcatDataset(corpus_builders[c]))
+                total += datasets[-1].num_samples
+                corpus_weights_achieved[c] =  float(datasets[-1].num_samples)/train_num_samples                
+                print_rank_0(f"    {c}: {datasets[-1].num_samples} w={corpus_weights_achieved[c]} (expected: {corpus_weights[c]})")
+
+            print_rank_0(f" > total number of samples: {total}")                
             print_rank_0(f" >>> Finished building {dataset_type} corpus datasets in {time.time() - start_time} seconds")
-            return datasets, [corpus_weights[c] for c in corpus_list]
+            return datasets, [corpus_weights_achieved[c] for c in corpus_list]
 
         if a > 0:
             train_datasets, train_weights = build_corpus_datasets('train')