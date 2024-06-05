b1f17d598d9bd6541879c13c2e016c4b6afe2341
Huihuo Zheng <zhenghh04@gmail.com>
2024-06-04 23:33:49 -0500

    fixed bug

--- ALCF/test_blendable_dataset.py
+++ ALCF/test_blendable_dataset.py
@@ -27,7 +27,7 @@ else:
     extra_trace_path=''
 
 os.makedirs(args.trace_dir, exist_ok=True)
-PerfTrace.initialize_log(f"{args.trace_dir}/trace-{ez.get_rank()}-of-{ez.get_world_size()}.pfw",  f"{args.data_cache_path}:{extra_trace_path}:{args.data_path}:{args.save}:{args.load}", process_id=comm.rank)
+PerfTrace.initialize_log(f"{args.trace_dir}/trace-{comm.rank}-of-{comm.size}.pfw",  f"{args.data_cache_path}:{extra_trace_path}:{args.data_path}:{args.save}:{args.load}", process_id=comm.rank)
 
 data_file_list = args.data_file_list
 print_rank_0(f"Reading data from {args.data_file_list}")
